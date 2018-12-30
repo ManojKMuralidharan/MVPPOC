abfde5fe13fd9c5c904b7f0821bf4741b31d3618
Manoj.Muralidharan <manoj.muralidharan@ril.com>
2018-12-30 22:01:07 +0530

    Unit test setup

--- app/src/main/java/practice/manu/mvppoc/main/MainContract.java
+++ app/src/main/java/practice/manu/mvppoc/main/MainContract.java
@@ -1,13 +1,17 @@
 package practice.manu.mvppoc.main;
 
 /**
- * Defines the contract between the view {@link MainActivity} and the presenter {@link MainPreseneter}
+ * Defines the contract between the view {@link MainActivity} and the presenter {@link MainPresenter}
  */
 public interface MainContract {
     interface MvpView{
+        void showSignInScreen();
 
+        void showSignUpScreen();
     }
     interface Presenter{
+        void handleSignInButtonClick();
 
+        void handleSignUpButtonClick();
     }
 }
